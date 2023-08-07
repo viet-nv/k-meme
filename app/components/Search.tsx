5a9bd02d03720e3f2c71b497e5d7f680d09b3538
Michael <108736931+michaelmuthomi@users.noreply.github.com>
2023-08-07 14:51:39 +0300

    Update Search.tsx

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -3,6 +3,7 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { useCallback, useEffect, useState } from "react";
 import { authors } from "../constants";
+import '../globals.css';
 
 function useDebounce(effect: any, dependencies: any, delay: number) {
   const callback = useCallback(effect, dependencies);
@@ -51,11 +52,13 @@ export default function Search() {
           setSearchVal(e.target.value);
         }}
         className="p-3 rounded-xl w-full flex-[2]"
+        id="inputBox"
         placeholder="Search content..."
       />
 
       <select
         className="p-3 rounded-xl flex-[1]"
+        id="inputBox"
         onChange={(e) => {
           router.push(
             pathname + "?" + createQueryString("author", e.target.value),
@@ -64,7 +67,7 @@ export default function Search() {
       >
         <option value="">All</option>
         {authors.map((item) => (
-          <option value={item} key={item}>
+          <option id="inputBox" value={item} key={item}>
             {item}
           </option>
         ))}
